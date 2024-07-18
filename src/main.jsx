cd64cb75cc19edaac815b17f640ae3f439ee238e
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-07-18 22:27:18 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={storeAppState}>
-      <HashRouter basename="/films/">
+      <HashRouter >
         <App />
       </HashRouter>
     </Provider>
