55bed37832c8be6001a917a2966c4185f5070cd4
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-07-18 15:48:31 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { storeAppState } from "./redux/store";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename="films" >
