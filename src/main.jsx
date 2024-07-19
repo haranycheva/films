00a03aeb9da1cb9fb995df8dd4f4967a99217795
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-07-19 09:54:12 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={storeAppState}>
-      <BrowserRouter basename="/films/">
+      <BrowserRouter basename="/films-react/">
         <App />
       </BrowserRouter>
     </Provider>
